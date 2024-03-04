d1605293ac234a0212de40299b1ce29f538f89ed
Yashveer Singh Makhloga <119954921+Yashveer325007@users.noreply.github.com>
2024-03-04 22:00:10 +0530

    Create neg_pos.py

---
+++ neg_pos.py
@@ -0,0 +1,7 @@
+num=int(input("Enter a number"))
+if(num>0):
+    print("The given number is positive")
+elif(num<0):
+    print("The given number is negative")
+else:
+    print("The given number is zero")
